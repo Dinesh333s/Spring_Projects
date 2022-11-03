8f075a7c18f99d9898f740f53d813a5db524779a
Dinesh <30436687+Dinesh333s@users.noreply.github.com>
2022-11-03 12:37:53 +0530

    Initial commit

---
+++ SpringMVC/src/test/java/com/floppy/springmvcboot/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floppy.springmvcboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
